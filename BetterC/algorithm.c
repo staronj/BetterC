7312a468ad793f80c05099691529bdf9e2c5874f
Jakub Staroń <staron.jk@gmail.com>
2016-04-04 11:07:25 +0200

    Adds algorithm.h and algorithm.c

---
+++ BetterC/algorithm.c
@@ -0,0 +1,115 @@
+/**
+ * Copyright Jakub Staroń 2015 - 2016
+ */
+
+#include "algorithm.h"
+#include "safe_memory_operations.h"
+
+#include <stdlib.h>
+#include <assert.h>
+
+int signum(int n) {
+  return (n < 0) ? -1 : ((n > 0) ? 1 : 0);
+}
+
+void sort(void* table, size_t count, size_t elementSize, ComparatorFunctionType comparator) {
+  assert(table != NULL);
+  assert(comparator != NULL);
+  assert(elementSize > 0);
+
+  qsort(table, count, elementSize, comparator);
+}
+
+size_t lower_bound_index(const void* table, size_t count, size_t elementSize, const void* value,
+                         ComparatorFunctionType comparator) {
+  assert(table != NULL);
+  assert(value != NULL);
+  assert(comparator != NULL);
+  assert(elementSize > 0);
+
+  size_t begin = 0;
+  size_t end = count;
+  /* Invariant: result is in range [being, end] */
+  while (begin < end) {
+    /* Because its integer division, middle is always less than end if begin < end. */
+    size_t middle = (begin + end) / 2;
+    const void* element = safe_raw_offset((void*)table, middle, elementSize);
+    int compare = comparator(element, value);
+    if (compare < 0)
+      begin = middle + 1;
+    else
+      end = middle;
+  }
+  return begin;
+}
+
+size_t upper_bound_index(const void* table, size_t count, size_t elementSize, const void* value,
+                         ComparatorFunctionType comparator) {
+  assert(table != NULL);
+  assert(value != NULL);
+  assert(comparator != NULL);
+  assert(elementSize > 0);
+
+  size_t begin = 0;
+  size_t end = count;
+  /* Invariant: result is in range [being, end] */
+  while (begin < end) {
+    /* Because its integer division, middle is always less than end if begin < end. */
+    size_t middle = (begin + end) / 2;
+    const void* element = safe_raw_offset((void*)table, middle, elementSize);
+    int compare = comparator(element, value);
+    if (compare <= 0)
+      begin = middle + 1;
+    else
+      end = middle;
+  }
+  return begin;
+}
+
+bool binary_search(const void* table, size_t count, size_t elementSize, const void* value,
+                   ComparatorFunctionType comparator) {
+  assert(table != NULL);
+  assert(value != NULL);
+  assert(comparator != NULL);
+  assert(elementSize > 0);
+
+  size_t index = lower_bound_index(table, count, elementSize, value, comparator);
+  void* element = safe_raw_offset((void*)table, index, elementSize);
+  return (count > 0 && comparator(value, element) == 0);
+}
+
+size_t minimum_element_index(const void* table, size_t count, size_t elementSize, ComparatorFunctionType comparator) {
+  assert(table != NULL);
+  assert(count > 0);
+  assert(comparator != NULL);
+  assert(elementSize > 0);
+
+  const void* element = table;
+  size_t index = 0;
+  for (size_t i = 1 ; i < count ; i++) {
+    const void* new_element = safe_raw_offset((void*)table, i, elementSize);
+    if (comparator(new_element, element) < 0) {
+      index = i;
+      element = new_element;
+    }
+  }
+  return index;
+}
+
+size_t maximum_element_index(const void* table, size_t count, size_t elementSize, ComparatorFunctionType comparator) {
+  assert(table != NULL);
+  assert(count > 0);
+  assert(comparator != NULL);
+  assert(elementSize > 0);
+
+  const void* element = table;
+  size_t index = 0;
+  for (size_t i = 1 ; i < count ; i++) {
+    const void* new_element = safe_raw_offset((void*)table, i, elementSize);
+    if (comparator(element, new_element) < 0) {
+      index = i;
+      element = new_element;
+    }
+  }
+  return index;
+}
---
+++ BetterC/algorithm.h
@@ -0,0 +1,78 @@
+/**
+ * Copyright Jakub Staroń 2015 - 2016
+ */
+
+#ifndef BETTERC_ALGORITHM_H
+#define BETTERC_ALGORITHM_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#define MIN(a, b) ((a)<=(b)?(a):(b))
+#define MAX(a, b) ((a)>=(b)?(a):(b))
+
+
+/**
+ * Comparator function signature.
+ *
+ * Function takes two pointer to void which are pointers to arguments.
+ *
+ * When first argument is smaller should return -1.
+ * When arguments are equal should return 0;
+ * When second argument is smaller should return 1.
+ */
+typedef int (* ComparatorFunctionType)(const void*, const void*);
+
+/**
+ * Return signum of integet \p n.
+ *
+ * Signum is equal to -1 if n < 0, 0 if n == 0 and 1 if n > 0.
+ */
+int signum(int n);
+
+/**
+ * Sorts table.
+ *
+ * Sorts \p table with \p count elements of size \p elementSize with order defined by \p comparator.
+ */
+void sort(void* table, size_t count, size_t elementSize, ComparatorFunctionType comparator);
+
+/**
+ * Returns index to the first element in \p table which does not compare less than \p value.
+ *
+ * Returns \p count, if all elements are less.
+ */
+size_t lower_bound_index(const void* table, size_t count, size_t elementSize, const void* value,
+                         ComparatorFunctionType comparator);
+
+/**
+ * Returns index to the first element in \p table which compares greater than \p value.
+ *
+ * Returns \p count, if all elements are not greater.
+ */
+size_t upper_bound_index(const void* table, size_t count, size_t elementSize, const void* value,
+                         ComparatorFunctionType comparator);
+
+/**
+ * Test if value exists in sorted table.
+ *
+ * Returns true if any element in \p table is equivalent to \p value, and false otherwise.
+ */
+bool binary_search(const void* table, size_t count, size_t elementSize, const void* value,
+                   ComparatorFunctionType comparator);
+
+/**
+ * Returns index of minimum element in \p table.
+ *
+ * \p Count must be positive.
+ */
+size_t minimum_element_index(const void* table, size_t count, size_t elementSize, ComparatorFunctionType comparator);
+
+/**
+ * Returns index of maximum element in \p table.
+ *
+ * \p Count must be positive.
+ */
+size_t maximum_element_index(const void* table, size_t count, size_t elementSize, ComparatorFunctionType comparator);
+
+#endif /* BETTERC_ALGORITHM_H */
